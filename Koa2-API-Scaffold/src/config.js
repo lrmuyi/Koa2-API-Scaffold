9f590e96b3cca61d213d0c3b88656bc2cc6bc091
ruishan <wangruishan@fortube.com>
2021-09-15 18:06:45 +0800

    init

--- Koa2-API-Scaffold/src/config.js
+++ Koa2-API-Scaffold/src/config.js
@@ -15,11 +15,11 @@ export const System = {
 }
 
 export const DB = {
-  host: 'localhost', // 服务器地址
+  host: '127.0.0.1', // 服务器地址
   port: 3306, // 数据库端口号
-  username: 'admin', // 数据库用户名
-  password: 'admin888', // 数据库密码
-  database: 'development', // 数据库名称
+  username: 'root', // 数据库用户名
+  password: 'yourpassword', // 数据库密码
+  database: 'auto_trading_test', // 数据库名称
   prefix: 'api_' // 默认"api_"
 }
 
