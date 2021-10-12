93f05cfdcdb462d3db62802e71ed15d66e69ae7f
ruishan <wangruishan@fortube.com>
2021-10-12 18:31:30 +0800

    feat create order

--- Koa2-API-Scaffold/src/services/binance.demo.js
+++ Koa2-API-Scaffold/src/services/binance.demo.js
@@ -1,37 +1,85 @@
 const { Spot } = require('@binance/connector')
 const fetch = require('node-fetch')
-
-
-const apiKey = 'H2hr7bWmqfTbWNGkOF74lv4nmFP9XMAcwOziSFoTlI9HSiWmzs0V7u5lWBBjT6Rr'
-const apiSecret = 'BNncYdRzykX8G0HbCaL3FaymVGIEtv3nMqKUU5FkWSoqOcUsRfm760VUVZjmIFDV'
-const client = new Spot(apiKey, apiSecret, { baseURL: 'https://api1.binance.com' })
-
-// console.info(await client.account({ recvWindow: 2000 }))
-// // Place a new order
-// client.newOrder('BNBUSDT', 'BUY', 'LIMIT', {
-//     price: '350',
-//     quantity: 1,
-//     timeInForce: 'GTC'
-// }).then(response => client.logger.log(response.data))
-//     .catch(error => client.logger.error(error))
-
-//     // /sapi/v1/system/status
-
+const fs = require('fs')
+const { Console } = require('console')
 
 class Binance {
-    test = async () => {
-        const response = await fetch('https://api.github.com/users/github');
-        const data = await response.json();
-        client.logger.info(data)
+    constructor() {
+        this.testBaseURL = 'https://api.github.com/users/github'
+        this.baseURL = 'https://api1.binance.com'
+        this.apiKey = 'H2hr7bWmqfTbWNGkOF74lv4nmFP9XMAcwOziSFoTlI9HSiWmzs0V7u5lWBBjT6Rr'
+        this.apiSecret = 'BNncYdRzykX8G0HbCaL3FaymVGIEtv3nMqKUU5FkWSoqOcUsRfm760VUVZjmIFDV'
+        // make sure the logs/ folder is created beforehand
+        this.output = fs.createWriteStream('./logs/stdout.log')
+        this.errorOutput = fs.createWriteStream('./logs/stderr.log')
+
+        this.logger = new Console({ stdout: this.output, stderr: this.errorOutput })
+        // this.client = new Spot(this.apiKey, this.apiSecret, { logger: this.logger })
+        this.client = new Spot(this.apiKey, this.apiSecret)
+    }
+    _log(info) {
+        this.client.logger.info(info)
     }
 
-    account = async () => {
-        client.logger.info(client)
-        let response = await client.account({ recvWindow: 2000 })
-        client.logger.log(response.data)
+    async testFetch() {
+        const response = await fetch(this.testBaseURL);
+        const data = await response.json();
+        this._log(data)
     }
+
+    test() {
+        this._log(this.client)
+    }
+
+
+    /**
+     * @description 获取账号信息
+     */
+    async account() {
+        let response = await this.client.account()
+        this._log(response.data)
+    }
+
+    async exchangeInfo() {
+        this.client.exchangeInfo().then(response => this._log(response.data))
+    }
+    /**
+     * @description 挂单
+     */
+    creatOrder(symbolPair, side, type = "LIMIT", price, amount, options = {}) {
+        this.client.newOrder(symbolPair, side, type, {
+            price: price,
+            quantity: amount,
+            timeInForce: 'GTC'
+        }).then(response => this._log(response.data))
+            .catch(error => this._log(error))
+    }
+    /**
+     * @description 取消挂单
+     */
+    cancelOrder(symbolPair, options = {}) {
+        this.client.cancelOrder(symbolPair, options).then(response => this._log(response.data))
+            .catch(error => this._log(error))
+    }
+
+    /**
+     * @description 取消挂单
+     */
+     allOrders(symbolPair, options = {}) {
+        this.client.allOrders(symbolPair, options).then(response => this._log(response.data))
+            .catch(error => this._log(error))
+    }
+
+    /**
+     * @description 
+     */
+
 }
 const binance = new Binance
 
 
-binance.account()
+// binance.account()
+// binance.creatOrder('FTMUSDT', 'SELL', 'LIMIT', '4.4', 10)
+binance.allOrders('FTMUSDT')
+
+// binance.test()
